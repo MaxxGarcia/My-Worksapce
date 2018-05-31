6e8cf5fb67bd29e00b94c701571ad9e01a56a832
V School Student <student@Vs-Mac-Pro-5.local>
2018-05-31 17:01:16 -0600

    assignments through the day

---
+++ exercises/grocerystorejs/grocerystore.js
@@ -0,0 +1,27 @@
+var shopper = {
+    bread: "Wheat",
+    mayo: "Not miracle whip",
+    mustard: "Whatves",
+    debbies: 3,
+    turky: false,
+    deliTurky: true,
+    function changeBoolean(){
+        if (groceryCart[0] == "bread") {
+        let haveBread = true;
+        console.log("Yep I have bread")
+    } else {
+        let haveBread = false;
+        console.log("I forgot the bread")
+    }
+    changeBoolean();
+    let groceryCart = ["bread", "mayo", "mustard", "little debbies snack cakes", "deli turky (the good kind)"]
+    //groceryCart array
+}}
+    
+
+console.log(shopper);
+
+let greeting = "Hello and welcome to 'Sam's club'. Have you seen our hot deals on bot generated memes?"
+    //string
+let total = 0.00
+    //number, it's free cause you coupon so good. 
