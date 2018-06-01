1a07dacd5777c5570871597a787c0e24a98ac146
V School Student <student@Vs-Mac-Pro-5.local>
2018-05-31 18:00:12 -0600

    finished one more exercise

--- exercises/grocerystorejs/grocerystore.js
+++ exercises/grocerystorejs/grocerystore.js
@@ -1,27 +1,18 @@
-var shopper = {
-    bread: "Wheat",
-    mayo: "Not miracle whip",
-    mustard: "Whatves",
-    debbies: 3,
-    turky: false,
-    deliTurky: true,
-    function changeBoolean(){
-        if (groceryCart[0] == "bread") {
-        let haveBread = true;
-        console.log("Yep I have bread")
-    } else {
-        let haveBread = false;
-        console.log("I forgot the bread")
-    }
-    changeBoolean();
-    let groceryCart = ["bread", "mayo", "mustard", "little debbies snack cakes", "deli turky (the good kind)"]
+let shopper = {
+    groceryCart: ["bread", "mayo", "mustard", "little debbies snack cakes", "deli turkey (the good kind)"],
     //groceryCart array
-}}
-    
-
-console.log(shopper);
-
-let greeting = "Hello and welcome to 'Sam's club'. Have you seen our hot deals on bot generated memes?"
+    greeting: "Hello and welcome to Sams club. Have you seen our hot deals on bot generated memes?",
     //string
-let total = 0.00
+    price: 0,
+    whiteBread: false,
     //number, it's free cause you coupon so good. 
+    breadCheck: function changeBoolean(){
+        if (shopper.groceryCart[0] === "bread") {
+        return console.log("Yep I have bread");
+    } else {
+        return console.log("I forgot the bread")
+    };
+    
+    
+}};
+shopper.breadCheck(shopper.groceryCart);
