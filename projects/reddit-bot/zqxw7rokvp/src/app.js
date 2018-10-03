9583086ef02c2dac268714fc96895a0a86e50509
Maxx <maxx.v.garcia@gmail.com>
2018-10-02 21:24:28 -0600

    experimenting with reddit bot

--- projects/reddit-bot/zqxw7rokvp/src/app.js
+++ projects/reddit-bot/zqxw7rokvp/src/app.js
@@ -13,14 +13,12 @@ const r = new snoowrap({
 const client = new snoostorm(r);
 
 const streamOpts = {
-  subreddit: "testingground4bots",
-  results: 25
+  subreddit: "all",
+  results: 1
 };
 
-const posts = client.SubmissionStream(streamOpts);
+const comments = client.CommentStream(streamOpts);
 
-posts.on("posts", posts => {
-  if(post.body) {
-    post.reply("They aren't very active on Reddit anymore, try checking out their discord: https://discord.gg/Q4fekPQ \n They are on a bit of a hold right now from what I hear, but are hoping to keep the project moving. There are active games still, so I suggest hopping on the discord and asking around! \n (This action was posted as a bot response. Feel free to let me know if you want it removed.")
-  }
+comments.on("comments", comments => {
+  console.log(comments)
 });
